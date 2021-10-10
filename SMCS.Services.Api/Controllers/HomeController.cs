213231bfafcf42f0b22902abd7f719a65601b18e
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-10 13:59:21 -0700

    CONTROLLER: Home Get Endpoint

---
+++ SMCS.Services.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace SMCS.Services.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok(value: "Hello from SCMS Services Api.");
+    }
+}
