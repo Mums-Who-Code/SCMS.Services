ca21e6b57cf2828927ee7b7ef57a2e11acb5b6e4
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-10 13:17:49 -0700

    INFRA: Initiatlize Acceptance Test Project AB#917

---
+++ SCMS.Services.Tests.Acceptance/DeleteMe.cs
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Xunit;
+
+namespace SCMS.Services.Tests.Acceptance
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void Test1() =>
+            Assert.True(true);
+    }
+}
