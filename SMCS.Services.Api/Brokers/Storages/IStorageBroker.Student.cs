971eecfae5025c2430afa4b31f8b7eaf68d5f0ab
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-10 18:28:26 -0700

    BROKER: Insert Student AB#939

--- SMCS.Services.Api/Brokers/Storages/IStorageBroker.Student.cs
+++ SMCS.Services.Api/Brokers/Storages/IStorageBroker.Student.cs
@@ -2,9 +2,13 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using SMCS.Services.Api.Models.Foundations.Students;
+
 namespace SMCS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        public ValueTask<Student> InsertStudentAsync(Student student);
     }
 }
--- SMCS.Services.Api/Brokers/Storages/StorageBroker.Student.cs
+++ SMCS.Services.Api/Brokers/Storages/StorageBroker.Student.cs
@@ -2,7 +2,9 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SMCS.Services.Api.Models.Foundations.Students;
 
 namespace SMCS.Services.Api.Brokers.Storages
@@ -10,5 +12,15 @@ namespace SMCS.Services.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<Student> Students { get; set; }
+
+        public async ValueTask<Student> InsertStudentAsync(Student student)
+        {
+            EntityEntry<Student> studentEntityEntry =
+                await this.Students.AddAsync(entity: student);
+
+            await this.SaveChangesAsync();
+
+            return studentEntityEntry.Entity;
+        }
     }
 }
