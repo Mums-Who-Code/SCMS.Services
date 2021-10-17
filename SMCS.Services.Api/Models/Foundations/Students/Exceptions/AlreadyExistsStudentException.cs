d4d9f104e3a1ae037c74c87efe6c2eefb30c754a
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-17 01:00:03 -0700

    ShouldThrowValidationExceptionOnAddIfStudentAlreadyExistsAndLogItAsync -> FAIL

---
+++ SMCS.Services.Api/Models/Foundations/Students/Exceptions/AlreadyExistsStudentException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace SMCS.Services.Api.Models.Foundations.Students.Exceptions
+{
+    public class AlreadyExistsStudentException : Exception
+    {
+        public AlreadyExistsStudentException(Exception innerException)
+            : base(message: "Student with the same id already exists.", innerException)
+        { }
+    }
+}
