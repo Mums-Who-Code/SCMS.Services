19306b4f77a67d106c519411c0b985913f7350dd
Shri Humrudha Jagathisun <shrija@microsoft.com>
2021-10-17 01:47:12 -0700

    CONTROLLERS: POST Student

---
+++ SMCS.Services.Api/Controllers/StudentsController.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using SMCS.Services.Api.Models.Foundations.Students;
+using SMCS.Services.Api.Models.Foundations.Students.Exceptions;
+using SMCS.Services.Api.Services.Foundations.Students;
+
+namespace SMCS.Services.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StudentsController : RESTFulController
+    {
+        private readonly IStudentService studentService;
+
+        public StudentsController(IStudentService studentService) =>
+            this.studentService = studentService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Student>> PostStudentAsync(Student student)
+        {
+            try
+            {
+                Student createdStudent =
+                    await this.studentService.AddStudentAsync(student);
+
+                return Created(createdStudent);
+            }
+            catch (StudentValidationException studentValidationException)
+            {
+                return BadRequest(studentValidationException.InnerException);
+            }
+            catch (StudentDependencyException studentDependencyException)
+            {
+                return InternalServerError(studentDependencyException);
+            }
+            catch (StudentDependencyValidationException studentDependencyValidationException)
+                when (studentDependencyValidationException.InnerException is AlreadyExistsStudentException)
+            {
+                return Conflict(studentDependencyValidationException.InnerException);
+            }
+            catch (StudentServiceException studentServiceException)
+            {
+                return InternalServerError(studentServiceException);
+            }
+        }
+    }
+}
