5b931679cd2663e8179ed664f44ce5b9b93d707c
Shri Humrudha <shri@mumswhocode.com>
2021-10-20 23:13:14 -0700

    ShouldThrowValidationExceptionOnAddIfStudentIsInvalidAndLogItAsync -> FAIL

--- SCMS.Services.Tests.Unit/Services/Foundations/Schools/SchoolTests.Validations.Add.cs
+++ SCMS.Services.Tests.Unit/Services/Foundations/Schools/SchoolTests.Validations.Add.cs
@@ -45,5 +45,69 @@ namespace SCMS.Services.Tests.Unit.Services.Foundations.Schools
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+        public async Task ShouldThrowValidationExceptionOnAddIfStudentIsInvalidAndLogItAsync(
+            string invalidName)
+        {
+            // given
+            var invalidSchool = new School
+            {
+                Name = invalidName
+            };
+
+            var invalidSchoolException = new InvalidSchoolException();
+
+            invalidSchoolException.AddData(
+                key: nameof(School.Id),
+                values: "Id is required.");
+
+            invalidSchoolException.AddData(
+                key: nameof(School.Name),
+                values: "Text is required.");
+
+            invalidSchoolException.AddData(
+                key: nameof(School.CreatedDate),
+                values: "Date is required.");
+
+            invalidSchoolException.AddData(
+                key: nameof(School.UpdatedDate),
+                values: "Date is required.");
+
+            invalidSchoolException.AddData(
+                key: nameof(School.CreatedBy),
+                values: "Id is required.");
+
+            invalidSchoolException.AddData(
+                key: nameof(School.UpdatedBy),
+                values: "Id is required.");
+
+            var expectedSchoolValidationException =
+                new SchoolValidationException(invalidSchoolException);
+
+            // when
+            ValueTask<School> addSchoolTask =
+                this.schoolService.AddSchoolAsync(invalidSchool);
+
+            // then
+            await Assert.ThrowsAsync<SchoolValidationException>(() =>
+                addSchoolTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedSchoolValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertSchoolAsync(It.IsAny<School>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
---
+++ SMCS.Services.Api/Models/Foundations/Schools/Exceptions/InvalidSchoolException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace SMCS.Services.Api.Models.Foundations.Schools.Exceptions
+{
+    public class InvalidSchoolException : Xeption
+    {
+        public InvalidSchoolException()
+            : base(message: "Invalid school, fix the errors and try again.")
+        { }
+    }
+}
