9eb6d66f643344befb7e1bf27a7e58e14e504311
Shri Humrudha <shri@mumswhocode.com>
2021-10-24 19:33:25 -0700

    BROKER: Insert Student School

--- SMCS.Services.Api/Brokers/Storages/StorageBroker.StudentSchool.cs
+++ SMCS.Services.Api/Brokers/Storages/StorageBroker.StudentSchool.cs
@@ -2,7 +2,9 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SMCS.Services.Api.Models.Foundations.StudentSchools;
 
 namespace SMCS.Services.Api.Brokers.Storages
@@ -10,5 +12,17 @@ namespace SMCS.Services.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<StudentSchool> StudentSchools { get; set; }
+
+        public async ValueTask<StudentSchool> InsertStudentSchoolAsync(StudentSchool studentSchool)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<StudentSchool> entityEntry =
+                await broker.StudentSchools.AddAsync(studentSchool);
+
+            await broker.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
     }
 }
