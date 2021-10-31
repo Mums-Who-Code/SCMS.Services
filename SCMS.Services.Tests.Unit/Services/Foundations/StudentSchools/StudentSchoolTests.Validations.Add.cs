a1c02094d8ddafe6a31a6b1d1c08fb6bc9fcf05a
Shri Humrudha <shri@mumswhocode.com>
2021-10-31 08:00:30 -0700

    ShouldThrowValidationExceptionOnAddIfIdIsInvalidAndLogItAsync -> FAIL

--- SCMS.Services.Tests.Unit/Services/Foundations/StudentSchools/StudentSchoolTests.Validations.Add.cs
+++ SCMS.Services.Tests.Unit/Services/Foundations/StudentSchools/StudentSchoolTests.Validations.Add.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using Moq;
 using SMCS.Services.Api.Models.Foundations.StudentSchools;
@@ -28,6 +29,59 @@ namespace SCMS.Services.Tests.Unit.Services.Foundations.StudentSchools
             // when
             ValueTask<StudentSchool> addStudentSchoolTask =
                 this.studentSchoolService.AddStudentSchool(nullStudentSchool);
+
+            // then
+            await Assert.ThrowsAsync<StudentSchoolValidationException>(() =>
+                addStudentSchoolTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentSchoolValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentSchoolAsync(It.IsAny<StudentSchool>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfIdIsInvalidAndLogItAsync()
+        {
+            // given
+            Guid invalidId = Guid.Empty;
+
+            var invalidStudentSchool = new StudentSchool
+            {
+                Id = invalidId,
+                StudentId = invalidId,
+                SchoolId = invalidId
+            };
+
+            var invalidStudentSchoolException =
+                new InvalidStudentSchoolException();
+
+            invalidStudentSchoolException.AddData(
+                key: nameof(StudentSchool.Id),
+                values: "Id is required");
+
+            invalidStudentSchoolException.AddData(
+                key: nameof(StudentSchool.StudentId),
+                values: "Id is required");
+
+            invalidStudentSchoolException.AddData(
+                key: nameof(StudentSchool.SchoolId),
+                values: "Id is required");
+
+            var expectedStudentSchoolValidationException =
+                new StudentSchoolValidationException(
+                    invalidStudentSchoolException);
+
+            // when
+            ValueTask<StudentSchool> addStudentSchoolTask =
+                this.studentSchoolService.AddStudentSchool(invalidStudentSchool);
 
             // then
             await Assert.ThrowsAsync<StudentSchoolValidationException>(() =>
