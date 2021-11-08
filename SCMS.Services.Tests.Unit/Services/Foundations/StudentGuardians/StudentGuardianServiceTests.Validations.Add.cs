0cbfb27192e303ae4e7cff85fd5b0f40765e4c60
Shri Humrudha <shrija@microsoft.com>
2021-11-07 21:05:37 -0800

    ShouldThrowValidationExceptionOnAddIfStudentGuardianIsNullAndLogItAsync -> FAIL

---
+++ SCMS.Services.Tests.Unit/Services/Foundations/StudentGuardians/StudentGuardianServiceTests.Validations.Add.cs
@@ -0,0 +1,49 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Moq;
+using SCMS.Services.Api.Models.Foundations.StudentGuardians;
+using SCMS.Services.Api.Models.Foundations.StudentGuardians.Exceptions;
+using Xunit;
+
+namespace SCMS.Services.Tests.Unit.Services.Foundations.StudentGuardians
+{
+    public partial class StudentGuardianServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfStudentGuardianIsNullAndLogItAsync()
+        {
+            // given
+            StudentGuardian nullStudentGuardian = null;
+
+            var nullStudentGuardianException =
+                new NullStudentGuardianException();
+
+            var expectedStudentGuardianValidationException =
+                new StudentGuardianValidationException(nullStudentGuardianException);
+
+            // when
+            ValueTask<StudentGuardian> addStudentGuardianTask =
+                this.studentGuardianService.AddStudentGuardianAsync(nullStudentGuardian);
+
+            // then
+            await Assert.ThrowsAsync<StudentGuardianValidationException>(() =>
+                addStudentGuardianTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentGuardianValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentGuardianAsync(It.IsAny<StudentGuardian>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
