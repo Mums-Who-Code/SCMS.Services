fefc8b69d9771ebe73164770d1c1c278ba1d9949
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2021-11-12 20:45:08 +0330

    BROKER: Select All Student School

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.StudentSchool.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.StudentSchool.cs
@@ -2,13 +2,16 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
+using SCMS.Services.Api.Models.Foundations.Students;
 using SCMS.Services.Api.Models.Foundations.StudentSchools;
 
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
-        ValueTask<StudentSchool> InsertStudentSchoolAsync(StudentSchool studentSchool);
+        public ValueTask<StudentSchool> InsertStudentSchoolAsync(StudentSchool studentSchool);
+        public IQueryable<StudentSchool> SelectAllStudentSchools();
     }
 }
