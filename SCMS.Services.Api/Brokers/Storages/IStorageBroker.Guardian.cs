d3c4973ab373067395f1427ffbe827ef8514877f
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2021-11-12 21:17:42 +0330

    BROKER: Select All Guradian

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.Guardian.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.Guardian.cs
@@ -2,13 +2,16 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Linq;
 using System.Threading.Tasks;
 using SCMS.Services.Api.Models.Foundations.Guardians;
+using SCMS.Services.Api.Models.Foundations.Students;
 
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
-        public ValueTask<Guardian> InsertGuardianAsync(Guardian guardian);
+        ValueTask<Guardian> InsertGuardianAsync(Guardian guardian);
+        IQueryable<Guardian> SelectAllGuardians();
     }
 }
