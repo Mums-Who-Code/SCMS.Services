3b2ee793e1bfda7f71b27a4ed705e26730d8004a
Shri Humrudha <shrija@microsoft.com>
2021-11-14 22:18:39 -0800

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt -> FAIL

---
+++ SCMS.Services.Tests.Unit/Services/Foundations/StudentGuardians/StudentGuardianServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Microsoft.Data.SqlClient;
+using Moq;
+using SCMS.Services.Api.Models.Foundations.StudentGuardians.Exceptions;
+using Xunit;
+
+namespace SCMS.Services.Tests.Unit.Services.Foundations.StudentGuardians
+{
+    public partial class StudentGuardianServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStudentGuardianStorageException =
+                new FailedStudentGuardianStorageException(sqlException);
+
+            var expectedStudentGuardianDependencyException =
+                new StudentGuardianDependencyException(failedStudentGuardianStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllStudentGuardians())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllStudentGuardiansTask = () =>
+                this.studentGuardianService.RetrieveAllStudentGuardians();
+
+            // then
+            Assert.Throws<StudentGuardianDependencyException>(retrieveAllStudentGuardiansTask);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedStudentGuardianDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllStudentGuardians(),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
