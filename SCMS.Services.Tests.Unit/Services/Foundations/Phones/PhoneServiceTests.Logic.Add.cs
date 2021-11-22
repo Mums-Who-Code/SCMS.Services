86671904ce3376706ffaa61d7432017588d169e3
Shri Humrudha <shrija@microsoft.com>
2021-11-22 06:52:16 -0800

    ShouldAddPhoneAsync -> FAIL

---
+++ SCMS.Services.Tests.Unit/Services/Foundations/Phones/PhoneServiceTests.Logic.Add.cs
@@ -0,0 +1,45 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Force.DeepCloner;
+using Moq;
+using SCMS.Services.Api.Models.Foundations.Phones;
+using Xunit;
+
+namespace SCMS.Services.Tests.Unit.Services.Foundations.Phones
+{
+    public partial class PhoneServiceTests
+    {
+        [Fact]
+        public async Task ShouldAddPhoneAsync()
+        {
+            // given
+            Phone randomPhone = CreateRandomPhone();
+            Phone inputPhone = randomPhone;
+            Phone storagePhone = inputPhone;
+            Phone expectedPhone = storagePhone.DeepClone();
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.InsertPhoneAsync(inputPhone))
+                    .ReturnsAsync(storagePhone);
+
+            // when
+            Phone actualPhone = await this.phoneService
+                    .AddPhoneAsync(inputPhone);
+
+            // then
+            actualPhone.Should().BeEquivalentTo(expectedPhone);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertPhoneAsync(inputPhone),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
---
+++ SCMS.Services.Tests.Unit/Services/Foundations/Phones/PhoneServiceTests.cs
@@ -0,0 +1,56 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Moq;
+using SCMS.Services.Api.Brokers.DateTimes;
+using SCMS.Services.Api.Brokers.Loggings;
+using SCMS.Services.Api.Brokers.Storages;
+using SCMS.Services.Api.Models.Foundations.Phones;
+using SCMS.Services.Api.Services.Foundations.Phones;
+using Tynamix.ObjectFiller;
+
+namespace SCMS.Services.Tests.Unit.Services.Foundations.Phones
+{
+    public partial class PhoneServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<IDateTimeBroker> dateTimeBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IPhoneService phoneService;
+
+        public PhoneServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.dateTimeBrokerMock = new Mock<IDateTimeBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.phoneService = new PhoneService(
+                storageBroker: this.storageBrokerMock.Object,
+                dateTimeBroker: this.dateTimeBrokerMock.Object,
+                loggingBroker: this.loggingBrokerMock.Object);
+        }
+
+        private static DateTimeOffset GetRandomDateTime() =>
+            new DateTimeRange(earliestDate: new DateTime()).GetValue();
+
+        private static Phone CreateRandomPhone() =>
+            CreatePhoneFiller(dates: GetRandomDateTime()).Create();
+
+        private static Filler<Phone> CreatePhoneFiller(DateTimeOffset dates)
+        {
+            var filler = new Filler<Phone>();
+            Guid userId = Guid.NewGuid();
+
+            filler.Setup()
+                .OnType<DateTimeOffset>().Use(dates)
+                .OnType<Guid>().Use(userId)
+                .OnProperty(studentGuardian => studentGuardian.CreatedByUser).IgnoreIt()
+                .OnProperty(studentGuardian => studentGuardian.UpdatedByUser).IgnoreIt()
+                .OnProperty(studentGuardian => studentGuardian.Guardian).IgnoreIt();
+
+            return filler;
+        }
+    }
+}
