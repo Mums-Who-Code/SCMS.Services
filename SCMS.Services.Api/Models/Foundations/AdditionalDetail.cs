0c506a532018c233b27fa212b492c052c946f361
Ricky <rcruzfreelance@outlook.com>
2021-11-28 10:04:04 -0400

    DATA: Created Additional Details Model

---
+++ SCMS.Services.Api/Models/Foundations/AdditionalDetail.cs
@@ -0,0 +1,31 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using SCMS.Services.Api.Models.Foundations.Students;
+
+namespace SCMS.Services.Api.Models.Foundations
+{
+    public class AdditionalDetail
+    {
+        public Guid Id { get; set; }
+
+        public string Notes { get; set; }
+
+        public int Fide { get; set; }
+
+        public Guid StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTimeOffset UpdatedDate { get; set; }
+
+        public DateTimeOffset CreatedDate { get; set; }
+
+        public Guid UpdatedBy { get; set; }
+
+    }
+}
