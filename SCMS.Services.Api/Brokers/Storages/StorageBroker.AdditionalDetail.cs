a29c9acc1eacc72d6127d1d27741f6091b86f232
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2021-11-30 19:04:18 +0330

    BROKER: Insert Additional Details

--- SCMS.Services.Api/Brokers/Storages/StorageBroker.AdditionalDetail.cs
+++ SCMS.Services.Api/Brokers/Storages/StorageBroker.AdditionalDetail.cs
@@ -2,13 +2,27 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SCMS.Services.Api.Models.Foundations.AdditionalDetails;
 
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial class StorageBroker
     {
-        DbSet<AdditionalDetail> AddtionalDetails { get; set; }
+        DbSet<AdditionalDetail> AdditionalDetails { get; set; }
+
+        public async ValueTask<AdditionalDetail> InsertAdditionalDetailAsync(AdditionalDetail additionalDetail)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<AdditionalDetail> entityEntry =
+                await broker.AdditionalDetails.AddAsync(additionalDetail);
+
+            await broker.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
     }
 }
