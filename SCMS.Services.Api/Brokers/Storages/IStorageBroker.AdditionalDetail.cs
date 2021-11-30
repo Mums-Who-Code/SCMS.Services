1c1c590f571568342fa8daa2957c7d4021f16ebe
Muhammad.M Norouzi <Muhammadmnorouzi@Outlook.com>
2021-11-30 19:04:18 +0330

    BROKER: Insert Additional Details

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.AdditionalDetail.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.AdditionalDetail.cs
@@ -2,9 +2,14 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using SCMS.Services.Api.Models.Foundations.AdditionalDetails;
+using SCMS.Services.Api.Models.Foundations.Phones;
+using System.Threading.Tasks;
+
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<AdditionalDetail> InsertAdditionalDetailAsync(AdditionalDetail additionalDetail);
     }
 }
