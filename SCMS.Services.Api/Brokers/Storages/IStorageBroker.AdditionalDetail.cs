729ea97a5f608a224d117a5a76cc95cd7cb94529
Ricky <rcruzfreelance@outlook.com>
2021-12-06 22:52:04 -0400

    BROKER: Added missing method from interface

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.AdditionalDetail.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.AdditionalDetail.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using SCMS.Services.Api.Models.Foundations.AdditionalDetails;
@@ -12,5 +13,7 @@ namespace SCMS.Services.Api.Brokers.Storages
     {
         ValueTask<AdditionalDetail> InsertAdditionalDetailAsync(AdditionalDetail additionalDetail);
         IQueryable<AdditionalDetail> SelectAllAdditionalDetails();
+        ValueTask<AdditionalDetail> SelectAdditionalDetailByIdAsync(Guid additionalDetailId);
+        ValueTask<AdditionalDetail> UpdateAdditionalDetailAsync(AdditionalDetail additionalDetail
     }
 }
