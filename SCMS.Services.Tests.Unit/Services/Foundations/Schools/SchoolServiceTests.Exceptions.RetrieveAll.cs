0996a2ddcc7e625a2d40140fcef4937c3867b6e0
Shri Humrudha <shrija@microsoft.com>
2021-12-11 13:55:35 -0800

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync -> FAIL

---
+++ SCMS.Services.Tests.Unit/Services/Foundations/Schools/SchoolServiceTests.Exceptions.RetrieveAll.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using Microsoft.Data.SqlClient;
+using Moq;
+using SCMS.Services.Api.Models.Foundations.Schools.Exceptions;
+using Xunit;
+
+namespace SCMS.Services.Tests.Unit.Services.Foundations.Schools
+{
+    public partial class SchoolServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedSchoolStorageException =
+                new FailedSchoolStorageException(sqlException);
+
+            var expectedSchoolDependencyException =
+                new SchoolDependencyException(failedSchoolStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllSchools())
+                    .Throws(sqlException);
+
+            // when
+            Action retrieveAllSchoolsAction = () =>
+                this.schoolService.RetrieveAllSchools();
+
+            // then
+            Assert.Throws<SchoolDependencyException>(retrieveAllSchoolsAction);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllSchools(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedSchoolDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
