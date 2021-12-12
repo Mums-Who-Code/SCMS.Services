24ff50619d5c3cf9e7556c536d60d7d3d61c25a9
Shri Humrudha <shrija@microsoft.com>
2021-12-11 16:13:37 -0800

    PROCESSINGS: Cloud Management

---
+++ SCMS.Services.Infrastructure.Provision/Services/Processings/CloudManagements/CloudManagementProcessingService.cs
@@ -0,0 +1,92 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.Sql.Fluent;
+using SCMS.Services.Infrastructure.Provision.Brokers.Configurations;
+using SCMS.Services.Infrastructure.Provision.Models.Configurations;
+using SCMS.Services.Infrastructure.Provision.Models.Storages;
+using SCMS.Services.Infrastructure.Provision.Services.Foundations.CloudManagements;
+
+namespace SCMS.Services.Infrastructure.Provision.Services.Processings.CloudManagements
+{
+    internal class CloudManagementProcessingService : ICloudManagementProcessingService
+    {
+        private readonly ICloudManagementService cloudManagementService;
+        private readonly IConfigurationBroker configurationBroker;
+
+        public CloudManagementProcessingService()
+        {
+            cloudManagementService = new CloudManagementService();
+            this.configurationBroker = new ConfigurationBroker();
+        }
+
+        public async ValueTask ProcessAsync()
+        {
+            CloudManagementConfiguration cloudManagementConfiguration =
+                this.configurationBroker.GetConfigurations();
+
+            string projectName = cloudManagementConfiguration.ProjectName;
+            await ProvisionAsync(projectName, cloudAction: cloudManagementConfiguration.Up);
+            await DeprovisionAsync(projectName, cloudAction: cloudManagementConfiguration.Down);
+        }
+
+        private async ValueTask ProvisionAsync(string projectName, CloudAction cloudAction)
+        {
+            List<string> environments = RetrieveEnvironments(cloudAction);
+
+            foreach (string environment in environments)
+            {
+                IResourceGroup resourceGroup = await this.cloudManagementService
+                    .ProvisionResourceGroupAsync(
+                        projectName,
+                        environment);
+
+                ISqlServer sqlServer = await this.cloudManagementService
+                    .ProvisionSqlServerAsync(
+                        projectName,
+                        environment,
+                        resourceGroup);
+
+                SqlDatabase sqlDatabase = await this.cloudManagementService
+                    .ProvisionSqlDatabaseAsync(
+                        projectName,
+                        environment,
+                        sqlServer);
+
+                IAppServicePlan appServicePlan = await this.cloudManagementService
+                    .ProvisionPlanAsync(
+                        projectName,
+                        environment,
+                        resourceGroup);
+
+                IWebApp webApp = await this.cloudManagementService
+                    .ProvisionWebAppAsync(
+                        projectName,
+                        environment,
+                        databaseConnectionString: sqlDatabase.ConnectionString,
+                        resourceGroup,
+                        appServicePlan);
+            }
+        }
+
+        private async ValueTask DeprovisionAsync(string projectName, CloudAction cloudAction)
+        {
+            List<string> environments = RetrieveEnvironments(cloudAction);
+
+            foreach (string environment in environments)
+            {
+                await this.cloudManagementService.DeprovisionResourceGroupsAsync(
+                    projectName,
+                    environment);
+            }
+        }
+
+        private static List<string> RetrieveEnvironments(CloudAction cloudAction) =>
+            cloudAction?.Environments ?? new List<string>();
+    }
+}
---
+++ SCMS.Services.Infrastructure.Provision/Services/Processings/CloudManagements/ICloudManagementProcessingService.cs
@@ -0,0 +1,13 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+
+namespace SCMS.Services.Infrastructure.Provision.Services.Processings.CloudManagements
+{
+    internal interface ICloudManagementProcessingService
+    {
+        ValueTask ProcessAsync();
+    }
+}
