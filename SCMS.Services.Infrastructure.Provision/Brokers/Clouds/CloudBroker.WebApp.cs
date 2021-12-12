f7098a186dd550ea0f3ab7e432cce81c83ae8f0f
Shri Humrudha <shrija@microsoft.com>
2021-12-11 16:06:38 -0800

    BROKER: Web App

---
+++ SCMS.Services.Infrastructure.Provision/Brokers/Clouds/CloudBroker.WebApp.cs
@@ -0,0 +1,32 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.AppService.Fluent;
+using Microsoft.Azure.Management.AppService.Fluent.Models;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+
+namespace SCMS.Services.Infrastructure.Provision.Brokers.Clouds
+{
+    internal partial class CloudBroker
+    {
+        public async ValueTask<IWebApp> CreateWebAppAsync(
+            string webAppName,
+            string databaseConnectionString,
+            IAppServicePlan plan,
+            IResourceGroup resourceGroup)
+        {
+            return await azure.AppServices.WebApps
+                .Define(webAppName)
+                .WithExistingWindowsPlan(plan)
+                .WithExistingResourceGroup(resourceGroup)
+                .WithNetFrameworkVersion(NetFrameworkVersion.Parse("v6.0"))
+                .WithConnectionString(
+                    name: "DefaultConnection",
+                    value: databaseConnectionString,
+                    type: ConnectionStringType.SQLAzure)
+                .CreateAsync();
+        }
+    }
+}
