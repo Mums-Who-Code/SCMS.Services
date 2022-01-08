20c2a705f8d676edb94e4c9939346704e7e0346d
Shri Humrudha <shrija@microsoft.com>
2022-01-07 17:25:20 -0800

    ShouldRetrieveGuardianByIdAsync -> FAIL

--- SCMS.Services.Api/Services/Foundations/Guardians/GuardianService.cs
+++ SCMS.Services.Api/Services/Foundations/Guardians/GuardianService.cs
@@ -2,6 +2,7 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System;
 using System.Threading.Tasks;
 using SCMS.Services.Api.Brokers.DateTimes;
 using SCMS.Services.Api.Brokers.Loggings;
@@ -34,5 +35,7 @@ namespace SCMS.Services.Api.Services.Foundations.Guardians
             return await this.storageBroker.InsertGuardianAsync(guardian);
         });
 
+        public ValueTask<Guardian> RetrieveGuardianByIdAsync(Guid guardianId) =>
+            throw new NotImplementedException();
     }
 }
