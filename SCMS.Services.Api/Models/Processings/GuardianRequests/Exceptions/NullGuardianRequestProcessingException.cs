3e1ffcace5e592366d684b9e58a7156a4c56aed1
Shri Humrudha <shrija@microsoft.com>
2022-01-07 23:22:39 -0800

    ShouldThrowValidationExceptionOnEnsureIfGuardianRequestIsNullAndLogItAsync -> FAIL

---
+++ SCMS.Services.Api/Models/Processings/GuardianRequests/Exceptions/NullGuardianRequestProcessingException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace SCMS.Services.Api.Models.Processings.GuardianRequests.Exceptions
+{
+    public class NullGuardianRequestProcessingException : Xeption
+    {
+        public NullGuardianRequestProcessingException()
+            : base(message: "Guardian request is null.")
+        { }
+    }
+}
---
+++ SCMS.Services.Tests.Unit/Services/Processings/GuardianRequests/GuradianRequestsProcessingService.Validations.Ensure.cs
@@ -0,0 +1,52 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using SCMS.Services.Api.Models.Foundations.Guardians;
+using SCMS.Services.Api.Models.Processings.GuardianRequests;
+using SCMS.Services.Api.Models.Processings.GuardianRequests.Exceptions;
+using Xunit;
+
+namespace SCMS.Services.Tests.Unit.Services.Processings.GuardianRequests
+{
+    public partial class GuradianRequestsProcessingService
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnEnsureIfGuardianRequestIsNullAndLogItAsync()
+        {
+            // given
+            GuardianRequest nullGuardianRequest = null;
+
+            var nullGuardianRequestProcessingException =
+                new NullGuardianRequestProcessingException();
+
+            var expectedGuardianRequestValidationException =
+                new GuardianRequestProcessingValidationException(
+                    nullGuardianRequestProcessingException);
+
+            // when
+            ValueTask<GuardianRequest> ensureGuardianRequestExistsTask =
+                this.guardianRequestProcessingService
+                    .EnsureGuardianRequestExists(
+                        nullGuardianRequest);
+
+            // then
+            await Assert.ThrowsAsync<GuardianRequestProcessingValidationException>(() =>
+                ensureGuardianRequestExistsTask.AsTask());
+
+            this.guardianServiceMock.Verify(service =>
+                service.RetrieveGuardianByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.guardianServiceMock.Verify(service =>
+                service.AddGuardianAsync(It.IsAny<Guardian>()),
+                    Times.Once);
+
+            this.guardianServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
