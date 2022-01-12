0bf6562a9c2d57fc70f5f3d3c01f2e9121859e46
Shri Humrudha <shrija@microsoft.com>
2022-01-11 17:51:47 -0800

    ShouldThrowValidationExceptionOnEnsureIfGuardianRequestIsInvalidAndLogItAsync -> FAIL

---
+++ SCMS.Services.Api/Models/Processings/GuardianRequests/Exceptions/InvalidGuardianRequestProcessingException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace SCMS.Services.Api.Models.Processings.GuardianRequests.Exceptions
+{
+    public class InvalidGuardianRequestProcessingException : Xeption
+    {
+        public InvalidGuardianRequestProcessingException()
+            : base(message: "Invalid guardian request, fix the errors and try again.")
+        { }
+    }
+}
--- SCMS.Services.Tests.Unit/Services/Processings/GuardianRequests/GuradianRequestsProcessingService.Validations.Ensure.cs
+++ SCMS.Services.Tests.Unit/Services/Processings/GuardianRequests/GuradianRequestsProcessingService.Validations.Ensure.cs
@@ -53,5 +53,77 @@ namespace SCMS.Services.Tests.Unit.Services.Processings.GuardianRequests
             this.guardianServiceMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnEnsureIfGuardianRequestIsInvalidAndLogItAsync()
+        {
+            // given
+            var invalidGuardianRequest = new GuardianRequest();
+
+            var invalidGuardianRequestProcessingException = 
+                new InvalidGuardianRequestProcessingException();
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.Id),
+                values: "Id is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.FirstName),
+                values: "Text is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.LastName),
+                values: "Text is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.CountryCode),
+                values: "Text is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.Occupation),
+                values: "Text is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.CreatedDate),
+                values: "Date is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.CreatedBy),
+                values: "Id is required.");
+
+            invalidGuardianRequestProcessingException.AddData(
+                key: nameof(GuardianRequest.StudentId),
+                values: "Id is required.");
+
+            var expectedGuardianRequestProcessingValidationException =
+                new GuardianRequestProcessingValidationException(
+                    invalidGuardianRequestProcessingException);
+
+            // when
+            ValueTask<GuardianRequest> ensureGuardianRequestExistsTask =
+                this.guardianRequestProcessingService
+                    .EnsureGuardianRequestExists(
+                        invalidGuardianRequest);
+
+            // then
+            await Assert.ThrowsAsync<GuardianRequestProcessingValidationException>(() =>
+                ensureGuardianRequestExistsTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuardianRequestProcessingValidationException))),
+                        Times.Once);
+
+            this.guardianServiceMock.Verify(service =>
+                service.RetrieveGuardianByIdAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.guardianServiceMock.Verify(service =>
+                service.AddGuardianAsync(It.IsAny<Guardian>()),
+                    Times.Never);
+
+            this.guardianServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
