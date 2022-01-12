18cc58534e4ee1e81782281e8f4d32dfd64b2d51
Shri Humrudha <shrija@microsoft.com>
2022-01-11 18:33:33 -0800

    ShouldThrowDependencyValidationExceptionOnEnsureIfValidationErrorOccursAndLogItAsync -> FAIL

---
+++ SCMS.Services.Tests.Unit/Services/Processings/GuardianRequests/GuradianRequestsProcessingService.Exceptions.Ensure.cs
@@ -0,0 +1,64 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using System;
+using System.Threading.Tasks;
+using Moq;
+using SCMS.Services.Api.Models.Foundations.Guardians;
+using SCMS.Services.Api.Models.Foundations.Guardians.Exceptions;
+using SCMS.Services.Api.Models.Processings.GuardianRequests;
+using SCMS.Services.Api.Models.Processings.GuardianRequests.Exceptions;
+using Xeptions;
+using Xunit;
+
+namespace SCMS.Services.Tests.Unit.Services.Processings.GuardianRequests
+{
+    public partial class GuradianRequestsProcessingService
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnEnsureIfValidationErrorOccursAndLogItAsync()
+        {
+            // given
+            GuardianRequest someGuardianRequest = new GuardianRequest();
+            var someException = new Xeption();
+
+            var guardianValidationException =
+                new GuardianValidationException(someException);
+
+            var expectedGuardianRequestProcessingDependencyValidationException =
+                new GuardianRequestProcessingDependencyValidationException(
+                    guardianValidationException.InnerException as Xeption);
+
+            this.guardianServiceMock.Setup(service =>
+                service.RetrieveGuardianByIdAsync(It.IsAny<Guid>()))
+                    .ThrowsAsync(guardianValidationException);
+
+            // when
+            ValueTask<GuardianRequest> ensureGuardianRequestExistsTask =
+                this.guardianRequestProcessingService
+                    .EnsureGuardianRequestExists(
+                        someGuardianRequest);
+
+            // then
+            await Assert.ThrowsAsync<GuardianRequestProcessingDependencyValidationException>(() =>
+                ensureGuardianRequestExistsTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuardianRequestProcessingDependencyValidationException))),
+                        Times.Once);
+
+            this.guardianServiceMock.Verify(service =>
+                service.RetrieveGuardianByIdAsync(It.IsAny<Guid>()),
+                    Times.Once);
+
+            this.guardianServiceMock.Verify(service =>
+                service.AddGuardianAsync(It.IsAny<Guardian>()),
+                    Times.Never);
+
+            this.guardianServiceMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
