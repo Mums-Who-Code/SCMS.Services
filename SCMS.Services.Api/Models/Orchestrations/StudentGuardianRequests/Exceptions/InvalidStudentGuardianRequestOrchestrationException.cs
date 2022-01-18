9f6ea4dfbb69b81b2668288dbdce2fbd7f2f310b
Shri Humrudha <shrija@microsoft.com>
2022-01-17 16:35:24 -0800

    ShouldThrowValidationExceptionAddIfStudentGuardianRequestIsInvalidLogItAsync -> FAIL

---
+++ SCMS.Services.Api/Models/Orchestrations/StudentGuardianRequests/Exceptions/InvalidStudentGuardianRequestOrchestrationException.cs
@@ -0,0 +1,15 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using Xeptions;
+
+namespace SCMS.Services.Api.Models.Orchestrations.StudentGuardianRequests.Exceptions
+{
+    public class InvalidStudentGuardianRequestOrchestrationException : Xeption
+    {
+        public InvalidStudentGuardianRequestOrchestrationException()
+            : base(message: "Invalid student guardian request, fix the errors and try again.")
+        { }
+    }
+}
--- SCMS.Services.Tests.Unit/Services/Orchestrations/StudentGuardianRequests/StudentGuardianRequestOrchestrationServiceTests.Validations.Add.cs
+++ SCMS.Services.Tests.Unit/Services/Orchestrations/StudentGuardianRequests/StudentGuardianRequestOrchestrationServiceTests.Validations.Add.cs
@@ -19,7 +19,6 @@ namespace SCMS.Services.Tests.Unit.Services.Orchestrations.StudentGuardianReques
         {
             // given
             GuardianRequest nullGuardianRequest = null;
-            var mockSequence = new MockSequence();
 
             var nullStudentGuardianRequestOrchestrationException =
                 new NullStudentGuardianRequestOrchestrationException();
@@ -32,6 +31,99 @@ namespace SCMS.Services.Tests.Unit.Services.Orchestrations.StudentGuardianReques
             ValueTask<GuardianRequest> addStudentGuardianRequestTask =
                 this.studentGuardianRequestOrchestrationService
                     .AddStudentGuardianRequestAsync(nullGuardianRequest);
+
+            // then
+            await Assert.ThrowsAsync<StudentGuardianRequestOrchestrationValidationException>(() =>
+                addStudentGuardianRequestTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentGuardianRequestOrchestrationValidationException))),
+                        Times.Once);
+
+            this.studentProcessingServiceMock.Verify(service =>
+                service.VerifyStudentExistsAsync(It.IsAny<Guid>()),
+                    Times.Never);
+
+            this.guardianRequestProcessingServiceMock.Verify(service =>
+                service.EnsureGuardianRequestExists(It.IsAny<GuardianRequest>()),
+                    Times.Never);
+
+            this.studentGuardianProcessingServiceMock.Verify(service =>
+                service.AddStudentGuardianAsync(It.IsAny<StudentGuardian>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.studentProcessingServiceMock.VerifyNoOtherCalls();
+            this.guardianRequestProcessingServiceMock.VerifyNoOtherCalls();
+            this.studentGuardianProcessingServiceMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionAddIfStudentGuardianRequestIsInvalidLogItAsync()
+        {
+            // given
+            GuardianRequest invalidGuardianRequest = new GuardianRequest();
+
+            var invalidStudentGuardianRequestOrchestrationException =
+                new InvalidStudentGuardianRequestOrchestrationException();
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.Id),
+                values: "Id is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.FirstName),
+                values: "Text is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.LastName),
+                values: "Text is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.EmailId),
+                values: "Text is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.CountryCode),
+                values: "Text is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.ContactNumber),
+                values: "Text is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.Occupation),
+                values: "Text is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.StudentId),
+                values: "Id is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.CreatedDate),
+                values: "Date is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.UpdatedDate),
+                values: "Date is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.CreatedBy),
+                values: "Id is required.");
+
+            invalidStudentGuardianRequestOrchestrationException.AddData(
+                key: nameof(GuardianRequest.UpdatedBy),
+                values: "Id is required.");
+
+            var expectedStudentGuardianRequestOrchestrationValidationException =
+                new StudentGuardianRequestOrchestrationValidationException(
+                    invalidStudentGuardianRequestOrchestrationException);
+
+            // when
+            ValueTask<GuardianRequest> addStudentGuardianRequestTask =
+                this.studentGuardianRequestOrchestrationService
+                    .AddStudentGuardianRequestAsync(invalidGuardianRequest);
 
             // then
             await Assert.ThrowsAsync<StudentGuardianRequestOrchestrationValidationException>(() =>
