654a2ca800d17fbd0f2d93069a46b21df5b9f1c0
Janane <imjanane@gmail.com>
2022-02-20 00:43:43 -0600

    CODE RUB :  Istorage broker updated.

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.Branch.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.Branch.cs
@@ -2,9 +2,13 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using SCMS.Services.Api.Models.Foundations.Branches;
+
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<Branch> InsertStudentLevelAsync(Branch studentLevel);
     }
 }
