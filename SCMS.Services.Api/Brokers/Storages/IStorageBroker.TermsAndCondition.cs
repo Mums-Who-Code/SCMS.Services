f491f4f77b604a7c440b801ec6219200de8d3197
Shri Humrudha <shrija@microsoft.com>
2022-02-20 13:19:26 -0800

    BROKER: Insert Terms And Condition

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.TermsAndCondition.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.TermsAndCondition.cs
@@ -2,9 +2,13 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using SCMS.Services.Api.Models.Foundations.TermsAndConditions;
+
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
+        ValueTask<TermsAndCondition> InsertTermsAndConditionAsync(TermsAndCondition termsAndCondition);
     }
 }
--- SCMS.Services.Api/Brokers/Storages/StorageBroker.TermsAndCondition.cs
+++ SCMS.Services.Api/Brokers/Storages/StorageBroker.TermsAndCondition.cs
@@ -2,7 +2,9 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using SCMS.Services.Api.Models.Foundations.TermsAndConditions;
 
 namespace SCMS.Services.Api.Brokers.Storages
@@ -10,5 +12,15 @@ namespace SCMS.Services.Api.Brokers.Storages
     public partial class StorageBroker
     {
         public DbSet<TermsAndCondition> TermsAndConditions { get; set; }
+
+        public async ValueTask<TermsAndCondition> InsertTermsAndConditionAsync(TermsAndCondition termsAndCondition)
+        {
+            EntityEntry<TermsAndCondition> entityEntry =
+                await this.TermsAndConditions.AddAsync(entity: termsAndCondition);
+
+            await this.SaveChangesAsync();
+
+            return entityEntry.Entity;
+        }
     }
 }
