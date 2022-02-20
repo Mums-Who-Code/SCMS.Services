aed6c8e84d5ae4f9d7939330d93f09b075113f6b
Janane <imjanane@gmail.com>
2022-02-20 13:20:59 -0600

    CODE  RUB : Code Cleanup

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.Branch.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.Branch.cs
@@ -2,14 +2,6 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using SCMS.Services.Api.Models.Foundations.Branches;
-
 namespace SCMS.Services.Api.Brokers.Storages
 {
-    public partial interface IStorageBroker
-    {
-        DbSet<Branch> InsertStudentLevelAsync(Branch branch);
-    }
 }
--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.StudentLevel.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.StudentLevel.cs
@@ -3,13 +3,12 @@
 // -----------------------------------------------------------------------
 
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using SCMS.Services.Api.Models.Foundations.StudentLevels;
 
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
     {
-    DbSet<StudentLevel> InsertStudentLevelAsync(StudentLevel studentLevel);
+        ValueTask<StudentLevel> InsertStudentLevelAsync(StudentLevel studentLevel);
     }
 }
