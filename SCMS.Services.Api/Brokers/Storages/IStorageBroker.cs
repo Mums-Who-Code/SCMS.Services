0f5b55f635e682f41c4b464e010b22fceb2b12cb
Janane <imjanane@gmail.com>
2022-02-22 00:58:11 -0600

    BROKERS : Insert Branch Broker

--- SCMS.Services.Api/Brokers/Storages/IStorageBroker.cs
+++ SCMS.Services.Api/Brokers/Storages/IStorageBroker.cs
@@ -2,9 +2,16 @@
 // Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
 // -----------------------------------------------------------------------
 
+using System.Threading.Tasks;
+using SCMS.Services.Api.Models.Foundations.Branches;
+
 namespace SCMS.Services.Api.Brokers.Storages
 {
     public partial interface IStorageBroker
-    {
+    { 
+        public partial interface IStorageBroker
+        {
+            ValueTask<Branch> InsertStudentLevelAsync(Branch branch);
+        }
     }
 }
