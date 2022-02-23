1e9ef597297cca7969113b309bbe36b2c061c8fe
Janane <imjanane@gmail.com>
2022-02-22 22:59:56 -0600

    ShouldThrowValidationExceptionOnAddIfStudentLevelIsInvalidAndLogItAsync -> FAIL

--- SCMS.Services.Tests.Unit/Services/Foundations/StudentLevels/StudentLevelServiceTests.Validations.Add.cs
+++ SCMS.Services.Tests.Unit/Services/Foundations/StudentLevels/StudentLevelServiceTests.Validations.Add.cs
@@ -44,5 +44,62 @@ namespace SCMS.Services.Tests.Unit.Services.Foundations.StudentLevels
             this.storageBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
         }
+        
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("  ")]
+
+        public async Task ShouldThrowValidationExceptionOnAddIfStudentLevelIsInvalidAndLogItAsync(string invalidName)
+        {
+            // given
+            var invalidStudentLevel = new StudentLevel
+            {
+                Name = invalidName
+            };
+
+            var invalidStudentLevelException = new InvalidStudentLevelException();
+
+             invalidStudentLevelException.AddData(
+                key: nameof(StudentLevel.CreatedDate),
+                values: "Date is required.");
+
+            invalidStudentLevelException.AddData(
+                key: nameof(StudentLevel.UpdatedDate),
+                values: "Date is required.");
+
+            invalidStudentLevelException.AddData(
+                key: nameof(StudentLevel.CreatedBy),
+                values: "Id is required.");
+
+            invalidStudentLevelException.AddData(
+                key: nameof(StudentLevel.UpdatedBy),
+                values: "Id is required.");
+
+            var expectedStudentLevelValidationException =
+                new StudentLevelValidationException(invalidStudentLevelException);
+
+            // when
+            ValueTask<StudentLevel> addStudentLevelTask =
+                this.studentLevelService.AddStudentLevelAsync(invalidStudentLevel);
+
+            // then
+            await Assert.ThrowsAsync<StudentLevelValidationException>(() =>
+                addStudentLevelTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedStudentLevelValidationException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertStudentLevelAsync(It.IsAny<StudentLevel>()),
+                    Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
     }
 }
