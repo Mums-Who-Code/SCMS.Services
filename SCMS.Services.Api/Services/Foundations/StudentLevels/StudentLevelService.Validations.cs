0ecbf426d2bfcc291233bbdbf199ed9b7edee160
Tharani <tharanisantosh@outlook.com>
2022-02-23 05:35:25 +0200

    ShouldThrowValidationExceptionOnAddIfStudentLevelIsNullAndLogItAsync -> PASS

---
+++ SCMS.Services.Api/Services/Foundations/StudentLevels/StudentLevelService.Validations.cs
@@ -0,0 +1,20 @@
+﻿// -----------------------------------------------------------------------
+// Copyright (c) Signature Chess Club & MumsWhoCode. All rights reserved.
+// -----------------------------------------------------------------------
+
+using SCMS.Services.Api.Models.Foundations.StudentLevels;
+using SCMS.Services.Api.Models.Foundations.StudentLevels.Exceptions;
+
+namespace SCMS.Services.Api.Services.Foundations.StudentLevels
+{
+    public partial class StudentLevelService
+    {
+        private static void ValidateStudentLevel(StudentLevel studentLevel)
+        {
+            if (studentLevel == null)
+            {
+                throw new NullStudentLevelException();
+            }
+        }
+    }
+}
