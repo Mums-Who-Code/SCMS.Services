ec2eb249bbbfe4f3acf94adbebdbc0b6cde9d438
navjot kaur <kaurnavjot1983@outlook.com>
2022-03-04 11:33:42 -0800

    CONFIG: Update .Net

--- SCMS.Services.Infrastructure.Build/Program.cs
+++ SCMS.Services.Infrastructure.Build/Program.cs
@@ -52,7 +52,7 @@ namespace SCMS.Services.Infrastructure.Build
 
                                 TargetDotNetVersion = new TargetDotNetVersion
                                 {
-                                    DotNetVersion = "7.0.0-preview.1 ",
+                                    DotNetVersion = "7.0.100-preview.1.22110.4 ",
                                     IncludePrerelease = true
                                 },
                             },
