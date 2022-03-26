6bc30526a0915f2a16cf453b1cced2bef96a8a1d
Shri Humrudha <shrija@microsoft.com>
2022-03-26 07:30:22 -0700

    ShouldThrowDependencyValidationOnAddIfTermsAndConditionAlreadyExistsAndLogItAsync -> FAIL

--- SCMS.Services.Tests.Unit/Services/Foundations/TermsAndConditions/TermsAndConditionServiceTests.Exceptions.Add.cs
+++ SCMS.Services.Tests.Unit/Services/Foundations/TermsAndConditions/TermsAndConditionServiceTests.Exceptions.Add.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
 using Moq;
 using SCMS.Services.Api.Models.Foundations.TermsAndConditions;
@@ -57,6 +58,54 @@ namespace SCMS.Services.Tests.Unit.Services.Foundations.TermsAndConditions
 
             this.storageBrokerMock.Verify(broker =>
                 broker.InsertTermsAndConditionAsync(inputTermsAndCondition),
+                    Times.Never);
+
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationOnAddIfTermsAndConditionAlreadyExistsAndLogItAsync()
+        {
+            // given
+            TermsAndCondition randomTermsAndCondition = CreateRandomTermsAndCondition();
+            TermsAndCondition alreadyExistsTermsAndCondition = randomTermsAndCondition;
+            string someMessage = GetRandomString();
+            var duplicateKeyException = new DuplicateKeyException(someMessage);
+
+            var alreadyExistsTermsAndConditionException =
+                new AlreadyExistsTermsAndConditionException(
+                    duplicateKeyException);
+
+            var expectedTermsAndConditionDependencyValidation =
+                new TermsAndConditionDependencyValidationException(
+                    alreadyExistsTermsAndConditionException);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTime())
+                    .Throws(duplicateKeyException);
+
+            // when
+            ValueTask<TermsAndCondition> addTermsAndConditionTask =
+                this.termsAndConditionService.AddTermsAndConditionAsync(
+                    alreadyExistsTermsAndCondition);
+
+            // then
+            await Assert.ThrowsAsync<TermsAndConditionDependencyValidationException>(() =>
+                addTermsAndConditionTask.AsTask());
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTime(),
+                    Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedTermsAndConditionDependencyValidation))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.InsertTermsAndConditionAsync(It.IsAny<TermsAndCondition>()),
                     Times.Never);
 
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
