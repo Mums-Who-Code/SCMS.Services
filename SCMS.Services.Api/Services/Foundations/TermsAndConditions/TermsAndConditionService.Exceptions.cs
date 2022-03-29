4b19524786b939f8e7cad8b515b60d69ccf9e6cb
NaynaMallick <naynamallick@outlook.com>
2022-03-29 10:41:38 -0700

    ShouldThrowDependencyValidationOnAddIfTermsAndConditionAlreadyExistsAndLogItAsync -> PASS

--- SCMS.Services.Api/Services/Foundations/TermsAndConditions/TermsAndConditionService.Exceptions.cs
+++ SCMS.Services.Api/Services/Foundations/TermsAndConditions/TermsAndConditionService.Exceptions.cs
@@ -3,6 +3,7 @@
 // -----------------------------------------------------------------------
 
 using System.Threading.Tasks;
+using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
 using SCMS.Services.Api.Models.Foundations.TermsAndConditions;
 using SCMS.Services.Api.Models.Foundations.TermsAndConditions.Exceptions;
@@ -37,6 +38,14 @@ namespace SCMS.Services.Api.Services.Foundations.TermsAndConditions
                 throw CreateAndLogCriticalDependencyException(
                     failedTermsAndConditionStorageException);
             }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsTermsAndConditionException =
+                    new AlreadyExistsTermsAndConditionException(duplicateKeyException);
+
+                throw CreateAndLogDependencyValidationException(
+                    alreadyExistsTermsAndConditionException);
+            }
         }
 
         private TermsAndConditionValidationException CreateAndLogValidationException(Xeption exception)
@@ -57,6 +66,16 @@ namespace SCMS.Services.Api.Services.Foundations.TermsAndConditions
             this.loggingBroker.LogCritical(termsAndConditionDependencyException);
 
             return termsAndConditionDependencyException;
+        }
+
+        private TermsAndConditionDependencyValidationException CreateAndLogDependencyValidationException(Xeption exception)
+        {
+            var termsAndConditionDependencyValidationException =
+                new TermsAndConditionDependencyValidationException(exception);
+
+            this.loggingBroker.LogError(termsAndConditionDependencyValidationException);
+
+            return termsAndConditionDependencyValidationException;
         }
     }
 }
